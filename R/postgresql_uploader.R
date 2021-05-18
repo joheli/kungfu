71d5bd22dd97bee079b4285a3bfd040cae2efad9
Johannes Elias <joheli@gmx.net>
2021-05-18 13:51:56 +0000

    add postgresql_uploader and rbinder

---
+++ R/postgresql_uploader.R
@@ -0,0 +1,89 @@
+#' postgresql_uploader
+#'
+#' @param con a `DBI::DBIConnection` object.
+#' @param r_df data.frame to be uploaded; must contain identical column names to `pg_table`.
+#' @param pg_table character specifying PostgreSQL table name.
+#' @param unique.field.names character specifying unique column names.
+#' @param update logical specifying whether to update existing entries as defined by `unique.field.names`.
+#'
+#' @return list with information about effected inserts and updates
+#' @export
+postgresql_uploader <- function(con, r_df, pg_table, unique.field.names, update = TRUE) {
+  # extract field names - these *must* match postgresql table field names
+  fn <- colnames(r_df)
+
+  # check arguments
+  if (!all(unique.field.names %in% fn)) stop("'unique.field.names' must be a subset of 'colnames(r_df)'. Aborting.")
+
+  # create unique name for temporary postgres table
+  temp_table_name <- paste0("temp_",
+                            paste(sample(letters, 3), collapse = ""), # as timestamp is not collision resistant enough
+                            "_",
+                            format(Sys.time(), "%Y%m%d%H%M%OS"))
+
+  # sql for temporary table, which is just an empty copy of pg_table, which lives for the duration of a transaction
+  create_table_sql <- paste("CREATE TEMP TABLE", temp_table_name, "ON COMMIT DROP AS SELECT * FROM", pg_table, "WHERE FALSE")
+
+  # Start transaction
+  dbBegin(con)
+
+  # Create temporary table
+  DBI::dbSendQuery(con, create_table_sql)
+
+  # upload r_df into temporary table
+  postgresqlWriteTable(con = con, name = temp_table_name, value = r_df, append = TRUE, row.names = FALSE)
+
+  # prepare complicated insert SQL statement
+  insert_ <- paste("INSERT INTO", pg_table)
+  select_ <- paste("SELECT", paste(fn, collapse = ", "), "FROM", temp_table_name)
+  where_ <- paste("WHERE",
+                  paste(paste0(unique.field.names, " NOT IN (SELECT ", unique.field.names, " FROM ", pg_table, ")"),
+                        collapse = " AND "))
+  where2_ <- paste("WHERE",
+                  paste(paste0(unique.field.names, " IN (SELECT ", unique.field.names, " FROM ", pg_table, ")"),
+                        collapse = " OR "))
+  uploaded_sql <- paste(select_, where_)
+  not_uploaded_sql <- paste(select_, where2_)
+  insert_sql <- paste(insert_, select_, where_)
+
+  # extract info
+  uploaded <- dbFetch(dbSendQuery(con, uploaded_sql))
+  not_uploaded <- dbFetch(dbSendQuery(con, not_uploaded_sql))
+
+  # copy unique entries from temporary table to pg_table
+  status <- dbSendQuery(con, insert_sql)
+  info <- dbGetInfo(status)
+
+  # get some more info
+  info$uploaded <- uploaded
+  info$not_uploaded <- not_uploaded
+
+  # perform update (if update == TRUE)
+  if (update && nrow(not_uploaded) > 0) {
+    # prepare complicated update SQL statement
+    update_ <- paste("UPDATE", pg_table, "SET")
+    sets_ <- paste(paste(fn, "=", paste0(paste0(temp_table_name, "."), fn)), collapse = ", ")
+    from_ <- paste("FROM", temp_table_name, "WHERE")
+    where_ <- paste(paste(paste0(paste0(pg_table, "."), unique.field.names), "=", paste0(paste0(temp_table_name, "."), unique.field.names)), collapse = " AND ")
+    update_sql <- paste(update_, sets_, from_, where_)
+
+    # update
+    status2 <- dbSendQuery(con, update_sql)
+    info2 <- dbGetInfo(status2)
+
+    # merge info and info2
+    info <- c(info, info2)
+  }
+
+  # commit (and end) transaction
+  dbCommit(con)
+
+  # return
+  info
+}
+
+killConnections <- function() {
+  for(con in dbListConnections(dbDriver("PostgreSQL"))){
+    dbDisconnect(con)
+  }
+}
---
+++ R/rbinder.R
@@ -0,0 +1,104 @@
+#' rbinder
+#'
+#' Batch read and unite multiple data files into a single `data.frame`.
+#'
+#' @param file.pattern character; specifies regex pattern of file names to be processed.
+#' @param readf function; specifies function (which has to return a `data.frame`) used for reading in of data
+#' (defaults to `read.csv2`)
+#' @param path character; specifies directory from which to collect files corresponding to `file.pattern`
+#' (defaults to ".").
+#' @param unique.field.name character; column names designating unique entries that are not to be duplicated
+#' after uniting of files; use to avoid duplicates introduced by reading in of files with same content.
+#' @param result character; only used for debugging (in that case, pass "debug").
+#' @param ... arguments passed to function `readf`.
+#'
+#' @return a united data frame with unique entries not duplicated despite possible multiple occurrence in files
+#' @export
+rbinder <- function(file.pattern,
+                          readf = read.csv2,
+                          path = ".",
+                          unique.field.name,
+                          result = c("default",
+                                     "debug"),
+                          ...) {
+  result = match.arg(result)
+  step1 <- batchread(file.pattern,
+                           readf,
+                           path,
+                           ...)
+  step2 <- rbinder2(step1, unique.field.name)
+  rtn <- step2
+  if (result == "default") rtn <- step2[[1]]
+  return(rtn)
+}
+
+batchread <- function (file.pattern,
+                             readf = read.csv2,
+                             path = ".",
+                             ...) {
+  # file.names: a vector of matching file names
+  file.names <- dir(path, file.pattern)
+  # Stop if no matching files found
+  if (length(file.names) < 1) stop(paste0("No files matching the pattern '", file.pattern, "' found!"))
+  # create list to save data.frames
+  df.list <- list()
+  # loop, read in files to df.list
+  for (i in 1:length(file.names)) {
+    f.name <- file.names[i]
+    f.path <- f.name
+    if (path != ".") f.path <- paste0(path, f.name)
+    d <- readf(f.path, ...)
+    df.list[[f.name]] <- d
+  }
+  return(df.list)
+}
+
+rbinder2 <- function(df.list,
+                     unique.field.name) {
+  # stop if df.list is empty
+  if (length(df.list) < 1)
+    stop(paste0(deparse(substitute(df.list)), " appears to be empty!"))
+  # stop if df.list contents are not named
+  if (length(names(df.list)) < 1)
+    stop(paste0("Contents of ", deparse(substitute(df.list)), "are not named!"))
+  # stop if names of df.list contains NA
+  if (any(is.na(names(df.list))))
+    stop(paste0(deparse(substitute(df.list)), " has unnamed entries!"))
+
+  # d is the dataframe returned at the end
+  d <- data.frame()
+
+  # nrows holds the number of rows contained within each assessed data.frame
+  nrows <- numeric()
+
+  # uniquerows lists the number of unique rows contributed by each assessed data.frame
+  uniquerows  <- numeric()
+
+  # function for temporary key generation
+  tk <- function(df, u)
+    return(eval(parse(text = paste0("with(df, paste(", paste(u, collapse = ", "), "))"))))
+
+  # loop through the list of data.frames
+  for (df.name in names(df.list)) {
+    df.current <- df.list[[df.name]]      # current data.frame in loop
+    nrows  <- c(nrows, nrow(df.current))  # how much rows does the current data.frame have?
+    if (nrow(d) == 0) {
+      # if d is empty (at start), set d to df.current
+      d <- df.current
+      uniquerows <- nrows
+    } else {
+      # if d is not empty (after first iteration) reduce df.current to rows that are unique
+      df.current <- df.current[!(tk(df.current, unique.field.name) %in% tk(d, unique.field.name)), ]
+      uniquerows <- c(uniquerows, nrow(df.current))
+      d <- rbind(d, df.current)
+    }
+  }
+  report <- data.frame(`file name` = names(df.list), `number of rows` = nrows, `unique rows` = uniquerows)
+  report  <- rbind(report,
+                   data.frame(
+                     `file name` = "merged data.frame",
+                     `number of rows` = nrow(d),
+                     `unique rows` = length(unique(d[, unique.field.name]))
+                   ))
+  return(list(`merged data.frame` = d, report = report))
+}
